ca38b5c41f582376151d86b242c2fcc7acf9edb5
buzzrstudio <keepsoul@gmail.com>
2022-02-04 09:29:34 +0900

    Settings started

---
+++ src/components/common/BottomNavBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav>
+    <router-link>
+      <div class="icon"></div>
+      <span></span>
+    </router-link>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
