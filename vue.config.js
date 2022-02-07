090d22b7745ac753c6c505511785ef4e7c333353
buzzrstudio <keepsoul@gmail.com>
2022-02-07 17:04:48 +0900

    add: i18n

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  pluginOptions: {
+    i18n: {
+      locale: "ko",
+      fallbackLocale: "ko",
+      localeDir: "locales",
+      enableInSFC: false,
+      enableBridge: false,
+    },
+  },
+};
