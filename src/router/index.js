5ab184d08800e70f93bc2c59324b5e08ecaab970
buzzrstudio <keepsoul@gmail.com>
2022-04-21 13:39:20 +0900

    test

--- src/router/index.js
+++ src/router/index.js
@@ -339,9 +339,9 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   // 로그인 후 로그인페이지 접근 시
-  if (to.name === 'Login' || to.name === 'Join' || to.name === 'Agreement' || to.name === 'Join Complete') {
-    if ($store.state.accessToken) next('/');
-  }
+  // if (to.name === 'Login' || to.name === 'Join' || to.name === 'Agreement' || to.name === 'Join Complete') {
+  //   if ($store.state.accessToken) next('/');
+  // }
   // 권한이 필요한 페이지
   if (to.meta.auth) {
     if (!$store.state.accessToken) {
